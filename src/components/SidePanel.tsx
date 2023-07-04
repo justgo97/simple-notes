cac2e745536592961af9a589fa7c133f54b28b4b
hamdi2050 <hamdi.mar20@gmail.com>
2023-07-04 17:58:53 +0100

    small restyle 3

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -38,30 +38,32 @@ const SidePanel = ({ currentNoteID, handleCurrentNote }: SidePanelProps) => {
 
   return (
     <div className="app-side">
-      <h5 className="app-side-title">Notes list:</h5>
-      {Object.keys(notesList).map((noteID) => (
-        <div
-          className={`app-side-item border rounded p-1 ${
-            noteID === currentNoteID ? "app-side-item-selected" : ""
-          }`}
-          key={noteID}
-        >
+      <div className="p-3">
+        <h5 className="app-side-title">Notes list:</h5>
+        {Object.keys(notesList).map((noteID) => (
           <div
-            onClick={() => onClickItem(noteID)}
-            className="app-side-item-text"
+            className={`app-side-item border rounded p-1 ${
+              noteID === currentNoteID ? "app-side-item-selected" : ""
+            }`}
+            key={noteID}
           >
-            {notesList[noteID].title}
+            <div
+              onClick={() => onClickItem(noteID)}
+              className="app-side-item-text"
+            >
+              {notesList[noteID].title}
+            </div>
+            <div
+              onClick={() => onClickDelete(noteID)}
+              className="app-side-item-delete"
+            >
+              🗑️
+            </div>
           </div>
-          <div
-            onClick={() => onClickDelete(noteID)}
-            className="app-side-item-delete"
-          >
-            🗑️
-          </div>
+        ))}
+        <div onClick={onClickCreate} className="app-side-create pt-2">
+          + Create new note
         </div>
-      ))}
-      <div onClick={onClickCreate} className="app-side-create pt-2">
-        + Create new note
       </div>
     </div>
   );
